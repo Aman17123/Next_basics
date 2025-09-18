e8f66fe41fe7d22aff5fe9ac09a07060c888be24
wwwam <amannakoti0@gmail.com>
2025-09-19 00:14:57 +0530

    Not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Notfound() {
+  return(
+    <div className=" flex flex-col text-center text-5xl font-extrabold  text-violet-700">
+      <h1 className="text-center text-5xl text-violet-700">Page Not found</h1>
+      <p>Could not found request resource</p>
+    </div>
+  )
+}
