d88058fb4789088d0b662569a391f7078a49a75b
wwwam <amannakoti0@gmail.com>
2025-09-21 16:10:58 +0530

    File collaboration

---
+++ src/app/dashboard/cart.tsx
@@ -0,0 +1,9 @@
+function cart() {
+  return (
+    <div>
+      MEra bachpan gaya ............
+    </div>
+  );
+}
+
+export default cart;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+function dashboard() {
+  return (
+    <div className="text-3xl font-bold underline text-center  ">
+      "Aaisa bharosa khudpe college jaake degree li ni.. " -Calm
+    </div>
+  );
+}
+
+export default dashboard
