853d624413d21b5f3aad087898b2f0c91d5d6c77
wwwam <amannakoti0@gmail.com>
2025-09-23 17:33:01 +0530

    metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,12 @@
-function blog() {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog Page",
+  },
+};
+
+export default function blog() {
   return (
     <div>
       this is  blog page lil bro
@@ -6,4 +14,3 @@ function blog() {
   );
 }
 
-export default blog;
