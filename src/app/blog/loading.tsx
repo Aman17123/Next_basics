ea6b42dec9fd25363603ffce2221b496fcfdc3e1
wwwam <amannakoti0@gmail.com>
2025-11-21 20:35:44 +0530

    push

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -3,18 +3,14 @@
 import { motion } from "framer-motion";
 
 export default async function Loading() {
+
   await new Promise ((resolve) => {
     setTimeout( () => {
       resolve("HATT BSDK");
-    }, 9000 )
+    }, 2000 )
   })
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-700 text-white">
-      <motion.div
-        className="w-16 h-16 border-4 border-t-transparent border-white rounded-full"
-        animate={{ rotate: 360 }}
-        transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
-      />
       <motion.p
         className="mt-6 text-lg tracking-wide"
         initial={{ opacity: 0 }}
